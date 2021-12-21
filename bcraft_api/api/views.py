fa9e79290c0fdac8a1478315028b076d0a9a67e8
nidemidovich <demidovicnikita1@gmail.com>
2021-12-21 16:19:33 +0300

    Добавил сортировку по полям и файл requirements.txt

--- bcraft_api/api/views.py
+++ bcraft_api/api/views.py
@@ -17,7 +17,17 @@ class GetStatisticsForPeriodView(generics.ListAPIView):
     def get_queryset(self):
         start_date = self.kwargs.get('start_date')
         end_date = self.kwargs.get('end_date')
-        queryset = Statistics.objects.filter(date__gte=start_date, date__lte=end_date)
+        sort_column = self.request.query_params.get('sort_by')
+        if sort_column in [f.name for f in Statistics._meta.get_fields()]:
+            queryset = Statistics.objects.filter(
+                date__gte=start_date,
+                date__lte=end_date
+            ).order_by(sort_column)
+            return queryset
+
+        queryset = Statistics.objects.filter(
+            date__gte=start_date, date__lte=end_date
+        ).order_by('date')
         return queryset
 
 
@@ -28,4 +38,4 @@ class SendStatisticsView(generics.CreateAPIView):
 class DeleteStatisticsView(APIView):
     def delete(self, request):
         Statistics.objects.all().delete()
-        return Response('Success!')
+        return Response({'result': 'Success'})
